3e4a47d21a42c652a9add8163728d552f6124ab2
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-04 19:03:28 +0000

    #

--- my-app/src/lib/supabase/schema.ts
+++ my-app/src/lib/supabase/schema.ts
@@ -1,5 +1,23 @@
 import {
     pgTable,
+    text,
     timestamp,
     uuid,
 } from 'drizzle-orm/pg-core';
+
+export const workspaces = pgTable('workspaces', {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at', {
+      withTimezone: true,
+      mode: 'string',
+    })
+      .defaultNow()
+      .notNull(),
+    workspaceOwner: uuid('workspace_owner').notNull(),
+    title: text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    logo: text('logo'),
+    bannerUrl: text('banner_url'),
+  });
