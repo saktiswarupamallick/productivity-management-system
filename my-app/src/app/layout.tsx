d7483e026ed832af05be7070c3c0886531dd6d84
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-04 19:25:54 +0000

    #

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(db)
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
