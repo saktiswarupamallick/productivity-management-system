7fa549812dd23db983e7d773b3314ef5f4fa35c0
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-06 07:11:18 +0000

    #

--- my-app/src/app/(site)/layout.tsx
+++ my-app/src/app/(site)/layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      
+      {children}
+    </main>
+  );
 };
 
 export default HomePageLayout;
--- my-app/src/app/(site)/page.tsx
+++ my-app/src/app/(site)/page.tsx
@@ -1,6 +1,38 @@
 import React from "react";
+import TitleSection from '@/components/landing-page/title-section';
 
 const HomePage = () => {
-  return <div>f</div>;
+  return (
+    <section
+      className=" overflow-hidden
+  px-4
+  sm:px-6
+  mt-10
+  sm:flex
+  sm:flex-col
+  gap-4
+  md:justify-center
+  md:items-center"
+    >
+      <TitleSection
+        pill="✨ Your Workspace, Perfected"
+        title="All-In-One Collaboration and Productivity Platform"
+      />
+      <div
+        className="bg-white
+          p-[2px]
+          mt-6
+          rounded-xl
+          bg-gradient-to-r
+          from-primary
+          to-brand-primaryBlue
+          sm:w-[300px]
+        "
+      >
+       <Button></Button>
+      </div>
+    </section>
+  );
+
 };
 export default HomePage;
