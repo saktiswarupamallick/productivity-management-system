5d27351de1a9dbec7249103f48414838dc0a3432
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-06 12:34:20 +0000

    #

--- my-app/src/app/(site)/page.tsx
+++ my-app/src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import TitleSection from '@/components/landing-page/title-section';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
 
 const HomePage = () => {
   return (
@@ -29,7 +32,46 @@ const HomePage = () => {
           sm:w-[300px]
         "
       >
-       <Button></Button>
+        <Button
+          variant="btn-secondary"
+          className=" w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background
+          "
+        >
+          Get Cypress Free
+        </Button>
+      </div>
+      <div
+        className="md:mt-[-90px]
+          sm:w-full
+          w-[750px]
+          flex
+          justify-center
+          items-center
+          mt-[-40px]
+          relative
+          sm:ml-0
+          ml-[-50px]
+        "
+      >
+        <Image
+          src={Banner}
+          alt="Application Banner"
+        />
+        <div
+          className="bottom-0
+            top-[50%]
+            bg-gradient-to-t
+            dark:from-background
+            left-0
+            right-0
+            absolute
+            z-10
+          "
+        ></div>
       </div>
     </section>
   );
