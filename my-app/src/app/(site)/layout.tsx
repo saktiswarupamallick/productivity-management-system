1e143fd9680600eaeaa099b5c9705af536521e43
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-07 13:33:48 +0000

    #

--- my-app/src/app/(site)/layout.tsx
+++ my-app/src/app/(site)/layout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import Header from '@/components/landing-page/header';
+import React from 'react';
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      
+      <Header />
       {children}
     </main>
   );
