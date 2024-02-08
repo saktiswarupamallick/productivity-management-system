00e829594c8fa8beff72b852f8c575eb40bbb2ff
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-08 18:18:24 +0000

    #

--- my-app/src/lib/supabase/queries.tsx
+++ my-app/src/lib/supabase/queries.tsx
@@ -1,6 +1,17 @@
 'use server'
 import db from "./db";
-import { Subscription } from "./supabase.types";
+import { workspaces } from '../../../migrations/schema';
+import { Subscription , workspace } from "./supabase.types";
+
+export const createWorkspace = async (workspace: workspace) => {
+  try {
+    const response = await db.insert(workspaces).values(workspace);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
 
 export const getUserSubscriptionStatus = async (userId: string) => {
     try {
