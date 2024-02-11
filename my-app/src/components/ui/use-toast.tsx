d3f7f87bbd9bca6b8ea475b1e8b17edb4420b947
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-11 18:11:56 +0000

    #

--- my-app/src/components/ui/use-toast.tsx
+++ my-app/src/components/ui/use-toast.tsx
@@ -138,7 +138,7 @@ function dispatch(action: Action) {
   })
 }
 
-type Toast = Omit<ToasterToast, "id">
+type Toast = Omit<ToasterToast,"id">
 
 function toast({ ...props }: Toast) {
   const id = genId()
